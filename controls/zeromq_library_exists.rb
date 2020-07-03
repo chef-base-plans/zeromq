f2ccb4dcafb334424c1359efbb3cd848932af80d
Gavin Didrichsen <gavin.didrichsen@gmail.com>
2020-07-03 17:49:29 +0100

    Update documentation
    Signed-off-by: Gavin Didrichsen <gavin.didrichsen@gmail.com>

--- controls/zeromq_library_exists.rb
+++ controls/zeromq_library_exists.rb
@@ -7,8 +7,10 @@ control 'core-plans-zeromq-library-exists' do
   impact 1.0
   title 'Ensure zeromq library exists'
   desc '
-  Verify zeromq library by ensuring that (1) its installation directory exists; 
-  (2) the library exists; (3) its pkgconfig metadata contains the expected version
+  Verify zeromq library by ensuring that 
+  (1) its installation directory exists; 
+  (2) the library exists; 
+  (3) its pkgconfig metadata contains the expected version
   '
   
   plan_installation_directory = command("hab pkg path #{plan_origin}/#{plan_name}")
@@ -17,16 +19,15 @@ control 'core-plans-zeromq-library-exists' do
     its('stdout') { should_not be_empty }
   end
 
-  library_filename = input('library_filename', value: 'libzmq.so')
-  library_full_path = File.join(plan_installation_directory.stdout.strip, 'lib', "#{library_filename}")
+  library_filename = input('library_filename', value: 'zeromq.so')
+  library_full_path = File.join(plan_installation_directory.stdout.strip, 'lib', library_filename)
   describe file(library_full_path) do
     it { should exist }
   end
 
-  plan_pkg_ident = ((plan_installation_directory.stdout.strip).match /(?<=pkgs\/)(.*)/)[1]
-  plan_pkg_version = (plan_pkg_ident.match /^#{plan_origin}\/#{plan_name}\/(?<version>.*)\//)[:version]
-  pkgconfig_filename = input('pkgconfig_filename', value: 'libzmq.pc')
-  pkgconfig_full_path = File.join(plan_installation_directory.stdout.strip, 'lib', 'pkgconfig', "#{pkgconfig_filename}")
+  plan_pkg_version = plan_installation_directory.stdout.split("/")[5]
+  pkgconfig_filename = input('pkgconfig_filename', value: 'zeromq.pc')
+  pkgconfig_full_path = File.join(plan_installation_directory.stdout.strip, 'lib', 'pkgconfig', pkgconfig_filename)
   describe command("cat #{pkgconfig_full_path}") do
     its('exit_status') { should eq 0 }
     its('stdout') { should_not be_empty }
